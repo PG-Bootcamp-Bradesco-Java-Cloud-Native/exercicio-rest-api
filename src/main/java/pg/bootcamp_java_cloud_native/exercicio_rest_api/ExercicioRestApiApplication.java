cb9c31c224716e1ad0d4fd4918ca879401db9c59
Pedro Ghiotti <pedro.ghiotti.m@gmail.com>
2025-02-18 17:45:18 -0300

    Estrutura e dependências base.

---
+++ src/main/java/pg/bootcamp_java_cloud_native/exercicio_rest_api/ExercicioRestApiApplication.java
@@ -0,0 +1,13 @@
+package pg.bootcamp_java_cloud_native.exercicio_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pg/bootcamp_java_cloud_native/exercicio_rest_api/ExercicioRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pg.bootcamp_java_cloud_native.exercicio_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
